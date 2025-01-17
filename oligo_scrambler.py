9674b899f64bba90105d2ebebd8d8f10a403c060
Aaron Coley <aaron.coley@duke.edu>
2025-01-17 15:35:10 -0500

    freezing on run?

--- oligo_scrambler.py
+++ oligo_scrambler.py
@@ -1,25 +1,30 @@
 import random
+import pandas as pd
+
+random.seed(18)
 
 
 # need to scramble some oligo and shift as necessary
 
 # we're going tZo just create a list of the changed oligos and just keep adding to it
 
-test = "CAGATGCTCAACATTCCGCAGACCTCTCTGCAAGCAAAGCCCGTGGCCCCACAGGTGCCCAGCCCAGGGGGCGCCCCGGGCCAGGGTCCATACCCGTACAGCCTCTCTGAGCCAGCACCTCTCACTTTGGACACGAGCGGGAAGAATCTGACGGAGCAGAACAGCTACAGCAACATTCCTCACGAAGGGAAGCACACGCCGCTGTATGAGCGGTCCTTGCCCATCAACCCGGCCCAGAGCGGCAGCCCCAACCACGTGGATTCCGCCTACTTCCCTGGCTCTTCTACATCGTCATCTTCCGACAACGACGAGGGC"
+test = "XXXXXXXXXXXXXXXXXXXXXXXXX"
 scrambled_list = [] # add to this list with stuff
 scramble_size = 6   # we want to scramble 6 bases
-num_to_base = str.maketrans("1234", "ACGT")
+
 
 for i in range(0, len(test) - scramble_size + 1, 2):
     
     while True:
         
-        random_number = str(int(''.join(str(random.randint(1,4)) for num in range(scramble_size))))
-        replacement = random_number.translate(num_to_base)
+        replacement = test[i:i+scramble_size] # has all the og letters
+        temp = list(replacement) # list of characters
+        random.shuffle(temp) # order is randomized 
+        replacement = ''.join(temp)
         
         if (replacement != test[i:i+scramble_size]): # they arent equal to each other, aka we're actually doing something
             break
         
     scrambled_list.append(test[:i] + replacement + test[i+scramble_size:])
 
-print(scrambled_list[0])
+print(scrambled_list)
