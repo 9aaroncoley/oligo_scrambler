971132ab8a0172b917084844897a4c38bce1b771
Aaron Coley <aaron.coley@duke.edu>
2025-01-21 13:17:56 -0500

    fixed issue with # of og and scrambled wouldn't equal each other if the scramble would randomly equal the og

--- oligo_scrambler.py
+++ oligo_scrambler.py
@@ -21,10 +21,11 @@ count = -1
 
 for i in range(0, len(test) - scramble_size + 1, 2):
     count = count + 1
+    replacement = test[i:i+scramble_size] # has all the og letters
+    og_seq.append(replacement)
     while True:
         
-        replacement = test[i:i+scramble_size] # has all the og letters
-        og_seq.append(replacement)
+        
         temp = list(replacement) # list of characters
         random.shuffle(temp) # order is randomized 
         replacement = ''.join(temp)
@@ -32,7 +33,7 @@ for i in range(0, len(test) - scramble_size + 1, 2):
 
         if (replacement != test[i:i+scramble_size]): # they arent equal to each other, aka we're actually doing something
             break
-        if (replacement[0] * scramble_size == test[i:i+scramble_size]):
+        if (replacement[0] * scramble_size == test[i:i+scramble_size]): #if it's all just the same letter fr no scrambling can be done
             break
     id.append("FAM120A2P_" + str(count))
     bp_coordinates.append(str(i+1) + "-" + str(i+scramble_size)) 
@@ -42,6 +43,9 @@ for i in range(0, len(test) - scramble_size + 1, 2):
 #print(scrambled_seq)
 
 output_file = pd.DataFrame()
+
+
+
 output_file['ID'] = id
 output_file['og seq'] = og_seq
 output_file['scrambled seq'] = scrambled_seq
@@ -54,8 +58,8 @@ for string1, string2 in zip(og_seq, scrambled_seq):
         if (char1 != char2):
             hamming[count] = hamming[count] + 1
     count = count + 1
-output_file["Hamming Distance"] = hamming
 
+output_file["Hamming Distance"] = hamming
 
 output_file['Oligo'] = scrambled_list
 output_file_path = "output_locations.csv"
