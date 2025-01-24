bcf3bd67556fe6af5b73937a690d5b625d9b97d1
Aaron Coley <aaron.coley@duke.edu>
2025-01-24 12:36:24 -0500

    changed test variable to oligo

--- oligo_scrambler.py
+++ oligo_scrambler.py
@@ -8,7 +8,7 @@ random.seed(18)
 
 # we're going tZo just create a list of the changed oligos and just keep adding to it
 
-test = "CAGATGCTCAACATTCCGCAGACCTCTCTGCAAGCAAAGCCCGTGGCCCCACAGGTGCCCAGCCCAGGGGGCGCCCCGGGCCAGGGTCCATACCCGTACAGCCTCTCTGAGCCAGCACCTCTCACTTTGGACACGAGCGGGAAGAATCTGACGGAGCAGAACAGCTACAGCAACATTCCTCACGAAGGGAAGCACACGCCGCTGTATGAGCGGTCCTTGCCCATCAACCCGGCCCAGAGCGGCAGCCCCAACCACGTGGATTCCGCCTACTTCCCTGGCTCTTCTACATCGTCATCTTCCGACAACGACGAGGGC"
+oligo = "CAGATGCTCAACATTCCGCAGACCTCTCTGCAAGCAAAGCCCGTGGCCCCACAGGTGCCCAGCCCAGGGGGCGCCCCGGGCCAGGGTCCATACCCGTACAGCCTCTCTGAGCCAGCACCTCTCACTTTGGACACGAGCGGGAAGAATCTGACGGAGCAGAACAGCTACAGCAACATTCCTCACGAAGGGAAGCACACGCCGCTGTATGAGCGGTCCTTGCCCATCAACCCGGCCCAGAGCGGCAGCCCCAACCACGTGGATTCCGCCTACTTCCCTGGCTCTTCTACATCGTCATCTTCCGACAACGACGAGGGC"
 scrambled_list = [] # add to this list with stuff
 scramble_size = 6   # we want to scramble 6 bases
 id = []
@@ -19,9 +19,9 @@ scrambled_seq = []
 
 count = -1
 
-for i in range(0, len(test) - scramble_size + 1, 2):
+for i in range(0, len(oligo) - scramble_size + 1, 2):
     count = count + 1
-    replacement = test[i:i+scramble_size] # has all the og letters
+    replacement = oligo[i:i+scramble_size] # has all the og letters
     og_seq.append(replacement)
     while True:
         
@@ -31,14 +31,14 @@ for i in range(0, len(test) - scramble_size + 1, 2):
         replacement = ''.join(temp)
         # EDGE CASE - all bases are the same
 
-        if (replacement != test[i:i+scramble_size]): # they arent equal to each other, aka we're actually doing something
+        if (replacement != oligo[i:i+scramble_size]): # they arent equal to each other, aka we're actually doing something
             break
-        if (replacement[0] * scramble_size == test[i:i+scramble_size]): #if it's all just the same letter fr no scrambling can be done
+        if (replacement[0] * scramble_size == oligo[i:i+scramble_size]): #if it's all just the same letter fr no scrambling can be done
             break
     id.append("FAM120A2P_" + str(count))
     bp_coordinates.append(str(i+1) + "-" + str(i+scramble_size)) 
     scrambled_seq.append(replacement)
-    scrambled_list.append(test[:i] + replacement + test[i+scramble_size:])
+    scrambled_list.append(oligo[:i] + replacement + oligo[i+scramble_size:])
 
 #print(scrambled_seq)
 
